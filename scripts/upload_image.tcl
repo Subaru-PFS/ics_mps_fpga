050a9d4c654a65b6cc78ab410be9f1ee2f248263
Craig Loomis <cloomis@astro.princeton.edu>
2020-03-26 13:51:54 -0400

    Remove the JTAG serial number dependence in the upload script
    This does assume that there is only one JTAG bus on the hw_server.

--- scripts/upload_image.tcl
+++ scripts/upload_image.tcl
@@ -1,14 +1,17 @@
 puts stderr "connecting to hw_server at HW_HOST=$env(HW_HOST)"
 connect -url tcp:$env(HW_HOST):3121
 
+puts "found on JTAG bus:"
+puts [targets]
+
 puts "uploading hardware design: $env(ICS_MPS_FPGA_DIR)/pfs_ctrlr/pfs_ctrlr.sdk/block_design_wrapper_hw_platform_0/download.bit"
-targets -set -filter {jtag_cable_name =~ "Digilent JTAG-SMT1 210203A030DEA" && level==0} -index 0
+targets -set -filter {level==0} -index 0
 fpga -file $env(ICS_MPS_FPGA_DIR)/pfs_ctrlr/pfs_ctrlr.sdk/block_design_wrapper_hw_platform_0/download.bit
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A030DEA"} -index 0
+
+puts "resetting processor...."
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"} -index 0
 rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A030DEA"} -index 0
 
 puts stderr "uploading HLS image: $env(ICS_MPS_FPGA_DIR)/pfs_ctrlr/pfs_ctrlr.sdk/pfs_ctrlr/Release/pfs_ctrlr.elf"
 dow $env(ICS_MPS_FPGA_DIR)/pfs_ctrlr/pfs_ctrlr.sdk/pfs_ctrlr/Release/pfs_ctrlr.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A030DEA"} -index 0
 con
